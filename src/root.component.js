7df550dba919a076fb21eda3e3d19a20a193dfc1
Rohit Kumar Rana <rohitkumarrana67@gmail.com>
2020-12-27 16:07:31 +0530

    added basic footer

--- src/root.component.js
+++ src/root.component.js
@@ -1,5 +1,14 @@
 import React from "react";
+import {Navbar, Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Root(props) {
-  return <section>I am another micro-frontend</section>;
+  return (
+        <div>
+           <Navbar bg="primary" expand="lg" style={{color:"white",bottom:"0", width:"100%"}}>
+             i am the footer
+            </Navbar>
+        </div>
+  );
 }
